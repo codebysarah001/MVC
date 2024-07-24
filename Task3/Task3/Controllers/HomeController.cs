851d829a184b954347a2c81038c2543ce7d99b0a
codebysarah001 <sarahshormam@gmail.com>
2024-07-24 17:47:54 +0300

    updated

---
+++ Task3/Task3/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string validUsername = "user";
+        private const string validPassword = "password";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginInfo(string username, string password)
+        {
+            if (username == validUsername && password == validPassword)
+            {
+                // Save user info in session
+                Session["Username"] = username;
+                Session["Password"] = password;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid username or password.";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
