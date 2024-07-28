f56f7be117b3366eace71de7be8b7f6fc2186024
codebysarah001 <sarahshormam@gmail.com>
2024-07-28 17:45:43 +0300

    Updated

---
+++ Users/Users/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserDBEntities db = new UserDBEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+            return View(users);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Password,Image")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
